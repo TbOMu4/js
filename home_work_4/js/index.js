ba4ddfe06983a77411a70cb0db210111c05d2d89
TbOMu4 <artemtumoshenko@gmail.com>
2024-04-30 14:37:41 +0300

    home_work_4

---
+++ home_work_4/js/index.js
@@ -0,0 +1,35 @@
+"use strict";
+
+// Для вирішення цієї задачі вам буде потрібно згадати про перетворення у рядочкі та числа,
+//  а також використати декілька методів масивів, які ми розбирали на вебінарі:
+
+const currentMaxValue = 4589;
+let reverseMaxValue;
+
+reverseMaxValue = String(currentMaxValue).split("").map(Number);
+
+reverseMaxValue.sort((a, b) => {
+  if (a < b) {
+    return 1;
+  } else if (a > b) {
+    return -1;
+  }
+});
+
+reverseMaxValue = parseInt(reverseMaxValue.join(""));
+
+console.log(reverseMaxValue); // 9854
+console.log(typeof reverseMaxValue); // 'number'
+
+// Задача на знаходження добутку масиву чисел з
+//  невідомою глибиною вкладеності:
+
+const resultsArray = [1, 2, [3, [4]]];
+let productOfArray;
+
+// тут ваш код...
+productOfArray = resultsArray.flat(Infinity).reduce((prev, current) => {
+  return prev * current;
+});
+
+console.log(productOfArray); // 24
