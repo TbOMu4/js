79d7391257b4a191831889e9e35b94e4ca3d5828
TbOMu4 <artemtumoshenko@gmail.com>
2024-06-06 09:19:30 +0300

    home_work_9

--- home_work_9/js/index.js
+++ home_work_9/js/index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-let headerTwo = document.querySelector(".headerTwo");
+let headerTwo = document.querySelector("#headerTwo");
 let firstSection = document.querySelector(".firstSection");
 let listItem = document.querySelector("ul li:nth-child(5)");
-let hatredLevelBlock = document.querySelector("hatredLevelBlock");
+let hatredLevelBlock = document.querySelector(".hatredLevelBlock");
